5a689129719a60513b26cbe30c3a09b22d4ccdd8
Bereket Belete <bbelete@apposit.com>
2020-04-11 20:13:26 +0300

    initial commit

---
+++ src/main/java/com/gbet/licensingservice/model/License.java
@@ -0,0 +1,23 @@
+package com.gbet.licensingservice.model;
+
+public class License {
+
+    String organizationId;
+    String licenseId;
+
+    public String getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(String organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getLicenseId() {
+        return licenseId;
+    }
+
+    public void setLicenseId(String licenseId) {
+        this.licenseId = licenseId;
+    }
+}
